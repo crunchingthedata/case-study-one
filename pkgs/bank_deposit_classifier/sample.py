d7160d93698911681e8684428f4d257ee69b7b25
Kristen Keller <kristenkeller@Kristens-MBP.fios-router.home>
2021-04-29 10:15:37 -0400

    add upsample function to pkg

--- pkgs/bank_deposit_classifier/sample.py
+++ pkgs/bank_deposit_classifier/sample.py
@@ -1,2 +1,17 @@
-def print_hi():
-  return print('hi')
+from collections import Counter
+import math
+
+import pandas as pd
+
+
+def upsample_minority_class(data, outcome, p_minority):
+    outcome_counts = Counter(data[outcome])
+    majority_class, majority_count = outcome_counts.most_common()[0]
+    minority_class, minority_count = outcome_counts.most_common()[-1]
+    desired_total_count = math.ceil(majority_count/(1-p_minority))
+    n_samples = desired_total_count - majority_count - minority_count
+    samples = data \
+        .loc[data[outcome] == minority_class] \
+        .sample(n_samples, replace=True)
+    upsampled_data = pd.concat([data, samples])
+    return upsampled_data
