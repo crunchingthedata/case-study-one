c72888e2e3704758cdfca1b3e93028440f5ea632
Kristen Keller <kristenkeller@Kristens-MBP.fios-router.home>
2021-05-08 11:13:59 -0400

    parameterized tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import pandas as pd
 import pytest
 
@@ -11,9 +13,18 @@ def data():
         })
     return data
 
-def test_upsample_minority_class(data):
-    data_ = sample.upsample_minority_class(data, 'y', 0.5)
-    assert isinstance(data_, pd.DataFrame)
+@pytest.mark.parametrize("p", [(0.5), (0.75), (0.9)])
+def test_upsample_minority_class(data, p):
+    outcome = 'y'
+    outcome_counts = Counter(data[outcome])
+    minority_class, _ = outcome_counts.most_common()[-1]
+
+    data_ = sample.upsample_minority_class(data, outcome, p)
+    n_minority = data_.loc[data_[outcome] == minority_class].shape[0]
+    n_total = data_.shape[0]
+    p_minority = n_minority/n_total
+
+    assert p_minority == pytest.approx(p, rel=0.05)
 
 def test_upsample_minority_class_high_p(data):
     with pytest.raises(ValueError) as e:
