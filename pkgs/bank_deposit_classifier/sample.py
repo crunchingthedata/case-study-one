861468d461733ebcd1aedb5f6ee3f885efa368e4
Kristen Keller <kristenkeller@Kristens-MBP.fios-router.home>
2021-05-08 11:22:19 -0400

    update probability bounds

--- pkgs/bank_deposit_classifier/sample.py
+++ pkgs/bank_deposit_classifier/sample.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 
 def upsample_minority_class(data, outcome, p_minority):
-    def check_p_minority_bounds(p_minority):
-        if (p_minority > 1) or (p_minority < 0):
+    def check_p_minority_bounds(p_minority, p_minority_pre):
+        if (p_minority > 1) or (p_minority < p_minority_pre):
             msg = 'Proportion out of bounds! p_minority must be between ' \
-                    f'0 and 1, but value passed was {p_minority}.'
+                    f'{p_minority_pre} and 1, but value passed was {p_minority}.'
             raise ValueError(msg)
 
     def check_outcome_binary(data, outcome):
@@ -19,12 +19,12 @@ def upsample_minority_class(data, outcome, p_minority):
                     f'has {n_outcomes} classes'
             raise ValueError(msg)
 
-    check_p_minority_bounds(p_minority)
     check_outcome_binary(data, outcome)
-
     outcome_counts = Counter(data[outcome])
     majority_class, majority_count = outcome_counts.most_common()[0]
     minority_class, minority_count = outcome_counts.most_common()[-1]
+    p_minority_pre = minority_count / data.shape[0]
+    check_p_minority_bounds(p_minority, p_minority_pre)
     desired_total_count = math.ceil(majority_count/(1-p_minority))
     n_samples = desired_total_count - majority_count - minority_count
     samples = data \
