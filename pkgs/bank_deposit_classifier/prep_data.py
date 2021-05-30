8f6a3478367ac8c1861b7d80e3ab43eea55a47bf
Kristen Keller <kristenkeller@Kristens-MBP.fios-router.home>
2021-05-30 19:11:41 -0400

    create dataprepr class

--- pkgs/bank_deposit_classifier/prep_data.py
+++ pkgs/bank_deposit_classifier/prep_data.py
@@ -14,36 +14,46 @@ INTERMEDIATE_DATA_DIR = os.path.join(
     'data/intermediate'
     )
 
-def get_data(data_path, features = None):
-    data = pd.read_csv(data_path, sep=';')
-    if features:
-        data = data[features]
-    print(f'Data read from {data_path}')
-    return data
+class DataPrep:
+    def __init__(self, outcome, features=None, categorical_features=None, one_hot_encoder=None):
+        self._outcome = outcome
+        self._features = features
+        self._categorical_features = categorical_features
+        self._one_hot_encoder = one_hot_encoder
 
-def encode_data(data, one_hot_encoder = None, categorical_features = None):
-    if categorical_features:
-        categorial = data[categorical_features]
-    else:
-        categorical = data.select_dtypes(exclude=np.number)
-        categorical_features = categorical.columns
-    if not one_hot_encoder:
-        one_hot_encoder = preprocessing.OneHotEncoder(
-            sparse=False,
-            drop='first'
-            )
-    categorical = one_hot_encoder.fit_transform(categorical)
-    categorical = pd.DataFrame(categorical, columns=one_hot_encoder.get_feature_names(categorical_features))
-    continuous = data.select_dtypes(include=np.number)
-    data = pd.concat([categorical, continuous], axis=1)
-    return data, one_hot_encoder
+    def get_data(self, data_path):
+        data = pd.read_csv(data_path, sep=';')
+        if self._features:
+            data = data[self._features]
+        print(f'Data read from {data_path}')
+        return data
 
-def split_data(data, outcome):
-    sampled_data = upsample_minority_class(data, outcome, 0.5)
-    train, test = train_test_split(data, random_state=123, train_size=0.8)
-    return train, test
+    def encode_data(self, data):
+        if self._categorical_features:
+            categorial = data[self._categorical_features]
+        else:
+            categorical = data.select_dtypes(exclude=np.number)
+            categorical_features = categorical.columns
+        if self._one_hot_encoder:
+            one_hot_encoder = self._one_hot_encoder
+        else:
+            one_hot_encoder = preprocessing.OneHotEncoder(
+                sparse=False,
+                drop='first'
+                )
+        categorical = one_hot_encoder.fit_transform(categorical)
+        categorical_columns = one_hot_encoder.get_feature_names(categorical_features)
+        categorical = pd.DataFrame(categorical, columns=categorical_columns)
+        continuous = data.select_dtypes(include=np.number)
+        data = pd.concat([categorical, continuous], axis=1)
+        return data, one_hot_encoder
 
-def save_data(data, file_name = 'data.csv'):
-    path = os.path.join(INTERMEDIATE_DATA_DIR, file_name)
-    data.to_csv(path, index=False)
-    print(f'Data written to {path}')
+    def split_data(self, data):
+        sampled_data = upsample_minority_class(data, self._outcome, 0.5)
+        train, test = train_test_split(data, random_state=123, train_size=0.8)
+        return train, test
+
+    def save_data(self, data, file_name = 'data.csv'):
+        path = os.path.join(INTERMEDIATE_DATA_DIR, file_name)
+        data.to_csv(path, index=False)
+        print(f'Data written to {path}')
--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -15,8 +15,13 @@ data_path = os.path.join(INPUT_DATA_DIR, 'bank-additional-full.csv')
 features = None
 categorical_features = None
 
-data = get_data(data_path, features = features)
-data, encoder = encode_data(data, categorical_features)
-train, test = split_data(data, outcome)
-save_data(train, 'train.csv')
-save_data(test, 'test.csv')
+dp = DataPrep(
+    outcome=outcome,
+    features=features,
+    categorical_features=categorical_features
+    )
+data = dp.get_data(data_path)
+data, encoder = dp.encode_data(data)
+train, test = dp.split_data(data)
+dp.save_data(train, 'train.csv')
+dp.save_data(test, 'test.csv')
