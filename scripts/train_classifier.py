95b5d8c8001d5a4f857bcc3c6e18b7fa4ac5e2d4
Kristen Keller <kristenkeller@Kristens-MBP.fios-router.home>
2021-08-09 13:46:29 -0400

    read model training parameters from config

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -1,23 +1,34 @@
 import os
+import yaml
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score
 
 from bank_deposit_classifier.sample import upsample_minority_class
-from bank_deposit_classifier.prep_data import DATA_DIR
+from bank_deposit_classifier.prep_data import DATA_DIR, CONFIG_DIR
 
 
-outcome = 'y'
-n_estimators = 100
-max_features = 6
+config_path = 'train_model.yaml'
+test_path = 'intermediate/test.csv'
+train_path = 'intermediate/train.csv'
 
-test_path = os.path.join(DATA_DIR, 'intermediate/test.csv')
-train_path = os.path.join(DATA_DIR, 'intermediate/train.csv')
-test = pd.read_csv(test_path)
-train = pd.read_csv(train_path)
+# get parameters from config
+config_path_full = os.path.join(CONFIG_DIR, config_path)
+with open(config_path_full, 'r') as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+outcome = config.get('outcome')
+n_estimators = config.get('n_estimators')
+max_features = config.get('max_features')
+
+# get test and train data
+test_path_full = os.path.join(DATA_DIR, test_path)
+train_path_full = os.path.join(DATA_DIR, train_path)
+test = pd.read_csv(test_path_full)
+train = pd.read_csv(train_path_full)
 train_resampled = upsample_minority_class(train, outcome, 0.5)
 
+# train model
 rf = RandomForestClassifier(
     n_estimators = n_estimators,
     max_features = max_features,
@@ -25,6 +36,7 @@ rf = RandomForestClassifier(
     )
 rf.fit(train_resampled.drop(outcome, axis=1), train_resampled[outcome])
 
+# evaluate model
 train_predictions = rf.predict(train_resampled.drop(outcome, axis=1))
 test_predictions = rf.predict(test.drop(outcome, axis=1))
 train_auc = roc_auc_score(train_resampled[outcome], train_predictions)
