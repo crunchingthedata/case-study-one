dbfd6332159006f2535b5aabdf3b21d65e73d5e2
Kristen Keller <kristenkeller@Kristens-MBP.fios-router.home>
2021-08-11 08:47:38 -0400

    add an example of making predictions to our load_model script

--- scripts/load_model.py
+++ scripts/load_model.py
@@ -1,4 +1,9 @@
+import os
+
 import mlflow
+import pandas as pd
+
+from bank_deposit_classifier.prep_data import DATA_DIR
 
 # load a model via the model registry
 mlflow.set_tracking_uri('http://localhost:5000')
@@ -10,5 +15,14 @@ print(model)
 mlflow.set_tracking_uri('http://localhost:5000')
 mlflow.set_experiment('case-study-one-test')
 path = "runs:/427e70ff5e8c48aeb741f98e7dba42b4/model"
-model = mlflow.sklearn.load_model(path) 
+model = mlflow.sklearn.load_model(path)
 print(model)
+
+# make predictions 
+test_path = 'intermediate/test.csv'
+test_path_full = os.path.join(DATA_DIR, test_path)
+test = pd.read_csv(test_path_full) \
+    .drop(['y'], axis=1) \
+    .head(5)
+predictions = model.predict(test)
+print(predictions)
